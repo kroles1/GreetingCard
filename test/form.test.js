14398619e5d06fc9006b898771e6a3ab185689e2
Ami <ami1996_tm@live.co.uk>
2022-09-21 16:52:01 +0100

    Updated package.json

--- test/form.test.js
+++ test/form.test.js
@@ -10,17 +10,22 @@ describe('Testing index.html', () => {
     })
 
     test('it has a title', () => {
-        // let title = document.querySelector('title');
-        // expect(title).toBeTruthy();
+        let title = document.querySelector('title');
+        expect(title).toBeTruthy();
     })
-    test('h1 says something about lotr to begin with', () => {
-        // const h1 = document.querySelector('h1')
-        // expect(h1.innerHTML).toContain("The Lord of the rings")
+    test('there is a h1 tag in webpage', () => {
+        const h1 = document.querySelector('h1')
+        // expect(h1.innerHTML).toContain('')
+        expect(h1).toBeTruthy();
     })
     test('the h1 is updated when we hit the button', () => {
         // const btn = document.querySelector('#updateTitle');
         // btn.dispatchEvent(new dom.window.Event('click'));
         // const h1 = document.querySelector('h1');
         // expect(h1.innerHTML).toContain("you hit me");
+    })
+
+    test('', () => {
+        
     })
 })
